1c71f78467aa07d1a9ab1b00e606511981f3951d
livebcdev0603 <b.ceb.0603@gmail.com>
2023-03-22 23:01:31 +0100

    Tests the create product post API

--- product-service/src/test/java/com/programmingtechie/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/programmingtechie/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,65 @@
 package com.programmingtechie.productservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.programmingtechie.productservice.dto.ProductRequest;
+import com.programmingtechie.productservice.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.math.BigDecimal;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//signifies that this module would be used in the class
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	//need to manually specify mongo version needed. in this case, a docker image version
+	@Container //To specify it is a mongodb container. Static to statically access this property
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+
+	@Autowired
+	private MockMvc mockMvc; //creates a mock environment for the test
+	@Autowired
+	private ObjectMapper objectMapper; //to map data obtained to string
+	@Autowired
+	private ProductRepository productRepository;
+
+	//adding first method
+	@DynamicPropertySource //adds docker mongodb uri dynamically
+	static void setProperties(DynamicPropertyRegistry dymDynamicPropertyRegistry) {
+		dymDynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+	@Test //writing integrated test
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(productRequestString))
+				.andExpect(status().isCreated());
+		Assertions.assertEquals(1, productRepository.findAll().size());
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("iPhone 13")
+				.description("iPhone 13")
+				.price(BigDecimal.valueOf(1200))
+				.build();
+		//build() to create an object of type request
 	}
 
 }
