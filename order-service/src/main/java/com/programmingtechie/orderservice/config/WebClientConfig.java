74d52dc548a4136bb2ea5e193051fdcd9658377e
livebcdev0603 <b.ceb.0603@gmail.com>
2023-03-27 13:21:41 +0200

    webclient class implemented to act as a loadbalancer and create a client app

--- order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.programmingtechie.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,7 +9,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(){
-        return WebClient.builder().build();
+    @LoadBalanced //to be able to select one server if multiple server instances have been instatianted
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
