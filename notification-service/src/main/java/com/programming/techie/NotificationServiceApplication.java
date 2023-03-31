5013482062e40f081db03cab7c65666f6fd9fcca
livebcdev0603 <b.ceb.0603@gmail.com>
2023-04-01 01:43:20 +0200

    Notification service main class created and kafka listener implemented

---
+++ notification-service/src/main/java/com/programming/techie/NotificationServiceApplication.java
@@ -0,0 +1,23 @@
+package com.programming.techie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    // name of the topic in order service
+    // listens to the notification from order service
+    @KafkaListener(topics = "notificationTopic")
+    public void handleNotification(OrderPlacedEvent orderPlacedEvent) {
+        // send out an email notification
+        log.info("Received Notification for Order - {}", orderPlacedEvent.getOrderNumber());
+    }
+}
